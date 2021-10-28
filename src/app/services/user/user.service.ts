ec548d041e81aa8300887f4fc94916aa0653f284
pamei07 <p.meinhard@iks-gmbh.com>
2021-10-28 12:24:17 +0200

    Add UserService

---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {User} from "../../model/user";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private readonly surveyUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.surveyUrl = 'http://localhost:8080';
+  }
+
+  public saveUser(user: User) {
+    return this.http.post(this.surveyUrl + '/postUser', user);
+  }
+}
