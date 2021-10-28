cb08920d0332a79998356137be7768222e473a30
pamei07 <p.meinhard@iks-gmbh.com>
2021-10-28 08:46:45 +0200

    Create form in parent component of each QuestionGroup

--- src/app/components/answer-survey/answer-question-list/answer-question-list.component.ts
+++ src/app/components/answer-survey/answer-question-list/answer-question-list.component.ts
@@ -1,10 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {QuestionGroup} from "../../../model/question-group";
-import {FormArray, FormBuilder} from "@angular/forms";
-import {Answer} from "../../../model/answer";
-import {Question} from "../../../model/question";
+import {FormBuilder, FormGroup, FormGroupDirective} from "@angular/forms";
 import {AnswerService} from "../../../services/answer/answer.service";
-import {Checkbox} from "../../../model/checkbox";
 
 @Component({
   selector: 'answer-question-list',
@@ -14,96 +11,63 @@ import {Checkbox} from "../../../model/checkbox";
 export class AnswerQuestionListComponent implements OnInit {
 
   @Input() questionGroup!: QuestionGroup;
-  answerArray: Answer[] = [];
-
-  answerForm = this.fb.group({
-    answers: this.fb.array([])
-  });
-
-  get answers() {
-    return this.answerForm.get('answers') as FormArray;
-  }
+  @Input() questionGroupIndex!: number;
+  parentForm!: FormGroup;
 
   constructor(private fb: FormBuilder,
-              private answerService: AnswerService) {
+              private answerService: AnswerService,
+              private parentFormGroup: FormGroupDirective) {
   }
 
   ngOnInit() {
-    this.insertInputFields();
+    this.parentForm = this.parentFormGroup.control;
   }
 
-  /**
-   * Push an answer form for each question into the FormArray 'answers'
-   * There are 2 types of answer forms:
-   * 1. For questions with checkboxes:
-   *    --> another FormArray containing FormGroups with an attribute 'checked' to mark if a checkbox has been checked
-   *        and a 'text' attribute to capture the text input if available
-   * 2. For plain text questions:
-   *    --> a FormControl capturing the text input
-   */
-  insertInputFields() {
-    this.questionGroup.questions!.forEach((question, index) => {
-      if (question.hasCheckbox) {
-        this.answers.push(this.fb.array([]));
-        let checkboxFormArray = this.answers.at(index) as FormArray;
-        question.checkboxGroup?.checkboxes?.forEach(checkbox => {
-          checkboxFormArray.push(this.fb.group({
-            checked: false,
-            text: ''
-          }))
-        })
-        this.answers.setControl(index, checkboxFormArray);
-      } else {
-        this.answers.push(this.fb.control(''));
-      }
-    })
-    console.log(this.answerForm);
-  }
-
-  /**
-   * Create Answer objects for each question on submit.
-   * 1. For checkboxes:
-   *    --> Create an Answer object only if a checkbox has been checked
-   * 2. For text questions:
-   *    --> Create an Answer object only if a text field is not empty
-   */
-  onSubmit() {
-    console.log(this.answerForm)
-    let answer: Answer;
-    let currentQuestion: Question = new Question();
-    let currentCheckbox: Checkbox = new Checkbox();
-    this.answers.controls.forEach((input, questionIndex) => {
-      if (input.value instanceof Array) {
-        input.value.forEach((checkbox, checkboxIndex) => {
-          if (checkbox.checked == true || checkbox.checked == 'true') {
-            answer = new Answer();
-
-            if (checkbox.text !== '') {
-              answer.setText(checkbox.text);
-            }
-
-            currentQuestion = this.questionGroup.questions![questionIndex]
-            answer.setQuestion(currentQuestion);
-
-            currentCheckbox = this.questionGroup.questions![questionIndex].checkboxGroup!.checkboxes![checkboxIndex];
-            answer.setCheckbox(currentCheckbox);
-
-            this.answerArray.push(answer)
-          }
-        })
-      } else if (input.value !== '') {
-        answer = new Answer();
-
-        answer.setText(input.value);
-
-        currentQuestion = this.questionGroup.questions![questionIndex]
-        answer.setQuestion(currentQuestion);
-
-        this.answerArray.push(answer);
-      }
-    });
-    console.log(this.answerArray);
-    this.answerService.saveAnswers(this.answerArray).subscribe();
-    this.answerArray = [];
-  }
+//
+// /**
+//  * Create Answer objects for each question on submit.
+//  * 1. For checkboxes:
+//  *    --> Create an Answer object only if a checkbox has been checked
+//  * 2. For text questions:
+//  *    --> Create an Answer object only if a text field is not empty
+//  */
+// onSubmit() {
+//   console.log(this.answerForm)
+//   let answer: Answer;
+//   let currentQuestion: Question = new Question();
+//   let currentCheckbox: Checkbox = new Checkbox();
+//   this.answers.controls.forEach((input, questionIndex) => {
+//     if (input.value instanceof Array) {
+//       input.value.forEach((checkbox, checkboxIndex) => {
+//         if (checkbox.checked == true || checkbox.checked == 'true') {
+//           answer = new Answer();
+//
+//           if (checkbox.text !== '') {
+//             answer.setText(checkbox.text);
+//           }
+//
+//           currentQuestion = this.questionGroup.questions![questionIndex]
+//           answer.setQuestion(currentQuestion);
+//
+//           currentCheckbox = this.questionGroup.questions![questionIndex].checkboxGroup!.checkboxes![checkboxIndex];
+//           answer.setCheckbox(currentCheckbox);
+//
+//           this.answerArray.push(answer)
+//         }
+//       })
+//     } else if (input.value !== '') {
+//       answer = new Answer();
+//
+//       answer.setText(input.value);
+//
+//       currentQuestion = this.questionGroup.questions![questionIndex]
+//       answer.setQuestion(currentQuestion);
+//
+//       this.answerArray.push(answer);
+//     }
+//   });
+//   console.log(this.answerArray);
+//   this.answerService.saveAnswers(this.answerArray).subscribe();
+//   this.answerArray = [];
+// }
 }
--- src/app/components/answer-survey/answer-survey-participation/answer-survey-participation.component.ts
+++ src/app/components/answer-survey/answer-survey-participation/answer-survey-participation.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Survey} from "../../../model/survey";
 import {ActivatedRoute} from "@angular/router";
+import {Answer} from "../../../model/answer";
+import {FormArray, FormBuilder} from "@angular/forms";
+import {AnswerService} from "../../../services/answer/answer.service";
 
 @Component({
   selector: 'answer-survey-participation',
@@ -11,12 +14,67 @@ export class AnswerSurveyParticipationComponent implements OnInit {
 
   survey!: Survey;
   uuid: string | null;
+  answerArray: Answer[] = [];
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  answerForm = this.fb.group({
+    questionGroupsAnswers: this.fb.array([])
+  });
+
+  get questionGroupsAnswers() {
+    return this.answerForm.get('questionGroupsAnswers') as FormArray;
+  }
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private fb: FormBuilder,
+              private answerService: AnswerService) {
     this.uuid = activatedRoute.snapshot.queryParamMap.get('surveyUUID');
     this.survey = JSON.parse(<string>sessionStorage.getItem('survey' + this.uuid));
   }
 
   ngOnInit() {
+    this.insertInputFields();
+  }
+
+  /**
+   * Push an answer form for each question into the FormArray 'answers'
+   * There are 2 types of answer forms:
+   * 1. For questions with checkboxes:
+   *    --> another FormArray containing FormGroups with an attribute 'checked' to mark if a checkbox has been checked
+   *        and a 'text' attribute to capture the text input if available
+   * 2. For plain text questions:
+   *    --> a FormControl capturing the text input
+   */
+  insertInputFields() {
+    this.survey.questionGroups!.forEach((questionGroup, questionGroupIndex) => {
+
+      this.questionGroupsAnswers.push(this.fb.array([]));
+
+      let questionGroupAnswerFormArray = this.questionGroupsAnswers.at(questionGroupIndex) as FormArray;
+
+      questionGroup.questions!.forEach((question, questionIndex) => {
+        if (question.hasCheckbox) {
+          questionGroupAnswerFormArray.push(this.fb.array([]));
+          let checkboxFormArray = questionGroupAnswerFormArray.at(questionIndex) as FormArray;
+
+          question.checkboxGroup?.checkboxes?.forEach(checkbox => {
+            checkboxFormArray.push(this.fb.group({
+              checked: false,
+              text: ''
+            }))
+          })
+
+          questionGroupAnswerFormArray.setControl(questionIndex, checkboxFormArray);
+        } else {
+          questionGroupAnswerFormArray.push(this.fb.control(''));
+        }
+
+      })
+
+    })
+    console.log(this.answerForm)
+  }
+
+  onSubmit() {
+    console.log('test');
   }
 }
