c85cf17272a8c2a2652bc9cd0cd312df6488874b
pamei07 <p.meinhard@iks-gmbh.com>
2021-10-29 12:04:02 +0200

    Update comments on methods

--- src/app/components/answer-survey/answer-survey-participation/answer-survey-participation.component.ts
+++ src/app/components/answer-survey/answer-survey-participation/answer-survey-participation.component.ts
@@ -54,7 +54,7 @@ export class AnswerSurveyParticipationComponent implements OnInit {
    *                - questionGroupsFormArray (FormArray - Containing a FormArray for each questionGroup):
    *                    0: FormArray (First questionGroup - Array containing the form fields for each question)
    *                       0: FormControl (if text question)
-   *                       1: FormArray (if question with checkboxes - Array containing FormGroups for each checkbox)
+   *                       1: FormArray (if question with checkboxes and multipleSelect - Array containing FormGroups for each checkbox)
    *                           0: FormGroup (First checkbox)
    *                               - 'checked'
    *                               - 'text
@@ -65,13 +65,16 @@ export class AnswerSurveyParticipationComponent implements OnInit {
    *                           X: FormGroup (Xth checkbox)
    *                               - 'checked'
    *                               - 'text
+   *                       2: FormGroup (if question with checkboxes but not multipleSelect)
+   *                           - 'checkboxId'
+   *                           - 'text'
    *                       ...
-   *                       m: FormControl/FormGroup
+   *                       m: FormControl/FormGroup/FormArray
    *                    1: FormArray (Second questionGroup)
-   *                       0: FormControl/FormArray
-   *                       1: FormControl/FormArray
+   *                       0: FormControl/FormGroup/FormArray
+   *                       1: FormControl/FormGroup/FormArray
    *                       ...
-   *                       n: FormControl/FormArray
+   *                       n: FormControl/FormGroup/FormArray
    *                    ...
    *                    Z: FormArray (Zth questionGroup)
    *
@@ -140,9 +143,11 @@ export class AnswerSurveyParticipationComponent implements OnInit {
 
   /**
    * Create Answer objects for each question on submit.
-   * 1. For checkboxes:
+   * 1. For checkboxes with multipleSelect:
    *    --> Create an Answer object only if a checkbox has been checked
-   * 2. For text questions:
+   * 2. For checkboxes without multipleSelect:
+   *    --> Create an Answer object only if a checkbox-id is given
+   * 3. For text questions:
    *    --> Create an Answer object only if a text field is not empty
    */
   private postAnswers(user: User) {
