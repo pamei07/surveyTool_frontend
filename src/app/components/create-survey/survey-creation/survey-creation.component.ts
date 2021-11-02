48e9f03343ed7d6d148a89e5fc0101e1de2dfaac
pamei07 <p.meinhard@iks-gmbh.com>
2021-11-02 15:04:07 +0100

    Remove unused parameters

--- src/app/components/create-survey/survey-creation/survey-creation.component.ts
+++ src/app/components/create-survey/survey-creation/survey-creation.component.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {Survey} from "../../../model/survey";
-import {ActivatedRoute, Router} from "@angular/router";
-import {FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'survey-creation',
@@ -13,11 +11,8 @@ export class SurveyCreationComponent implements OnInit {
   survey!: Survey;
   basicInfoGiven = false;
 
-  constructor(private activatedRoute: ActivatedRoute,
-              private router: Router,
-              private fb: FormBuilder) {
+  constructor() {
     this.survey = new Survey();
-    this.survey.questionGroups = [];
   }
 
   ngOnInit() {
