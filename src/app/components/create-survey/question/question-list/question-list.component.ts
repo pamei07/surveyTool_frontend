7d66c540da15cbd65121004623419c9afbcc5f14
pamei07 <p.meinhard@iks-gmbh.com>
2021-11-03 08:57:03 +0100

    Refactor to reactive form

--- src/app/components/create-survey/question/question-list/question-list.component.ts
+++ src/app/components/create-survey/question/question-list/question-list.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Survey} from "../../../../model/survey";
+import {FormBuilder} from "@angular/forms";
+import {Question} from "../../../../model/question";
 
 @Component({
   selector: 'question-list',
@@ -8,10 +10,20 @@ import {Survey} from "../../../../model/survey";
 
 export class QuestionListComponent implements OnInit {
 
-  @Input() indexQuestionGroup!: number;
   @Input() survey!: Survey;
+  @Input() indexQuestionGroup!: number;
 
-  constructor() {
+  updateForm = this.fb.group({
+    text: [''],
+    required: false,
+    checkboxGroup: this.fb.group({
+      multipleSelect: false,
+      minSelect: [''],
+      maxSelect: ['']
+    })
+  })
+
+  constructor(private fb: FormBuilder) {
   }
 
   ngOnInit() {
@@ -19,11 +31,38 @@ export class QuestionListComponent implements OnInit {
 
   deleteQuestion(indexQuestion: number) {
     this.survey.questionGroups![this.indexQuestionGroup].questions!.splice(indexQuestion, 1);
-    sessionStorage.setItem('newSurvey', JSON.stringify(this.survey));
   }
 
   updateQuestion(indexQuestion: number) {
-    sessionStorage.setItem('newSurvey', JSON.stringify(this.survey));
+    let questionToUpdate = this.survey.questionGroups![this.indexQuestionGroup].questions![indexQuestion];
+    questionToUpdate.text = this.updateForm.value.text;
+    questionToUpdate.required = this.updateForm.value.required;
+
+    if (questionToUpdate.hasCheckbox) {
+      let checkboxGroupToUpdate = questionToUpdate.checkboxGroup!;
+      checkboxGroupToUpdate.multipleSelect = this.updateForm.value.checkboxGroup.multipleSelect;
+      checkboxGroupToUpdate.minSelect = this.updateForm.value.checkboxGroup.minSelect;
+      checkboxGroupToUpdate.maxSelect = this.updateForm.value.checkboxGroup.maxSelect;
+      questionToUpdate.checkboxGroup = checkboxGroupToUpdate;
+    }
+
+    this.survey.questionGroups![this.indexQuestionGroup].questions![indexQuestion] = questionToUpdate;
   }
 
+  openModal(question: Question) {
+    this.updateForm.patchValue({
+      text: question.text,
+      required: question.required,
+    })
+
+    if (question.hasCheckbox) {
+      this.updateForm.patchValue({
+        checkboxGroup: {
+          multipleSelect: question.checkboxGroup!.multipleSelect,
+          minSelect: question.checkboxGroup!.minSelect,
+          maxSelect: question.checkboxGroup!.maxSelect
+        }
+      })
+    }
+  }
 }
