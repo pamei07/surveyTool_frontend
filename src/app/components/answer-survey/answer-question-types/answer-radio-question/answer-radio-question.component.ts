7ba6845fc6a620b93f1e938a6602af3cae9d4412
pamei07 <p.meinhard@iks-gmbh.com>
2021-11-04 16:07:06 +0100

    Enable and disable text fields if corresponding radio button is clicked

--- src/app/components/answer-survey/answer-question-types/answer-radio-question/answer-radio-question.component.ts
+++ src/app/components/answer-survey/answer-question-types/answer-radio-question/answer-radio-question.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Question} from "../../../../model/question";
 import {FormGroup, FormGroupDirective} from "@angular/forms";
+import {Checkbox} from "../../../../model/checkbox";
 
 @Component({
   selector: 'answer-radio-question',
@@ -33,5 +34,26 @@ export class AnswerRadioQuestionComponent implements OnInit {
 
   ngOnInit() {
     this.parentForm = this.parentFormGroup.control;
+  }
+
+  disableEnableInput(currentCheckbox: Checkbox) {
+    // If checkbox clicked has a text field => enable input field and field in FormGroup
+    if (currentCheckbox.hasTextField) {
+      (document.getElementById('checkbox' + currentCheckbox.id + 'text')! as HTMLInputElement).disabled = false;
+      this.text?.enable();
+    }
+    // If checkbox clicked has no text field => disable field in FormGroup, so that 'required' validation does not get triggered
+    else if (!currentCheckbox.hasTextField) {
+      this.text?.disable();
+    }
+    // Disable text fields of every other checkbox (regardless if checkbox clicked has own text field)
+    this.question.checkboxGroup?.checkboxes.forEach(checkbox => {
+      if (checkbox.hasTextField && checkbox.id !== currentCheckbox.id) {
+        if (!((document.getElementById('checkbox' + checkbox.id + 'text')! as HTMLInputElement).disabled)) {
+          (document.getElementById('checkbox' + checkbox.id + 'text')! as HTMLInputElement).disabled = true;
+
+        }
+      }
+    })
   }
 }
