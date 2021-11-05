6a5e652e059f1658615a10c8fd174e8bb567adb8
pamei07 <p.meinhard@iks-gmbh.com>
2021-11-05 16:15:21 +0100

    Add default values for min-/maxSelect

--- src/app/model/checkbox-group.ts
+++ src/app/model/checkbox-group.ts
@@ -4,8 +4,8 @@ import {Checkbox} from "./checkbox";
 export class CheckboxGroup {
   id: number | undefined;
   multipleSelect: boolean | undefined;
-  minSelect: number | undefined;
-  maxSelect: number | undefined;
+  minSelect: number = 0;
+  maxSelect: number = 2;
   question: Question | undefined;
   checkboxes: Checkbox[] = [];
 }
