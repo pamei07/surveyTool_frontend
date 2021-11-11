e5b81d2d46a824f8ea3b9c6924be9f2085645b8e
pamei07 <p.meinhard@iks-gmbh.com>
2021-11-11 11:42:58 +0100

    Extract table component for checkbox question answers

---
+++ src/app/components/view-results/question-types/checkbox-question/answers-checkbox-question-table/answers-checkbox-question-table.component.ts
@@ -0,0 +1,42 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Question} from "../../../../../model/question";
+import {Answer} from "../../../../../model/answer";
+
+@Component({
+  selector: 'answers-checkbox-question-table',
+  templateUrl: 'answers-checkbox-question-table.component.html'
+})
+
+export class AnswersCheckboxQuestionTableComponent implements OnInit {
+  @Input() question!: Question;
+  @Input() answers!: Answer[];
+  @Input() numberOfUsersAnswering!: number;
+  @Input() votesForCheckboxes!: number[];
+  percentagesForCheckboxes: number[] = [];
+  checkboxesHaveTextAnswers: boolean = false;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.calculatePercentages();
+    this.checkIfTextAnswersAvailable();
+  }
+
+
+  private calculatePercentages() {
+    this.votesForCheckboxes.forEach((votes) => {
+      this.percentagesForCheckboxes.push(votes / this.numberOfUsersAnswering);
+    })
+  }
+
+  private checkIfTextAnswersAvailable() {
+    if (this.answers.some((answer) => answer.text !== null)) {
+      this.checkboxesHaveTextAnswers = true;
+    }
+  }
+
+  checkboxIsMostVoted(checkboxIndex: number) {
+    return (this.votesForCheckboxes[checkboxIndex] == Math.max.apply(null, this.votesForCheckboxes));
+  }
+}
