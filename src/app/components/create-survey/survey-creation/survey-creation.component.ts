c3432b2862792d86dafb7e1f41d832934c2ede5e
pamei07 <p.meinhard@iks-gmbh.com>
2021-11-12 11:04:13 +0100

    Adjust error message

--- src/app/components/create-survey/survey-creation/survey-creation.component.ts
+++ src/app/components/create-survey/survey-creation/survey-creation.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Survey} from "../../../model/survey";
+import {QuestionGroup} from "../../../model/question-group";
+import {Question} from "../../../model/question";
+import {CheckboxGroup} from "../../../model/checkbox-group";
+import {Checkbox} from "../../../model/checkbox";
 
 @Component({
   selector: 'survey-creation',
@@ -16,6 +20,38 @@ export class SurveyCreationComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.basicInfoGiven = true;
+    this.survey.name = "test";
+    this.survey.endDate = "2021-12-08T08:39:00";
+    this.survey.startDate = "2021-11-03T08:39:00";
+    let questionGroup = new QuestionGroup();
+    questionGroup.title = 'test block';
+    let questionCheckboxes = new Question();
+    questionCheckboxes.text = 'jo';
+    questionCheckboxes.hasCheckbox = true;
+    let checkboxGroup = new CheckboxGroup();
+    checkboxGroup.multipleSelect = true;
+    checkboxGroup.minSelect = 1;
+    checkboxGroup.maxSelect = 2;
+    let checkbox1 = new Checkbox();
+    checkbox1.text = 'ja';
+    let checkbox2 = new Checkbox();
+    checkbox2.text = 'nein';
+    let checkbox3 = new Checkbox();
+    checkbox3.text = 'vllt';
+
+    let questionWithoutCheckboxes = new Question();
+    questionWithoutCheckboxes.text = 'test';
+    questionWithoutCheckboxes.hasCheckbox = true;
+    let checkboxGroup2 = new CheckboxGroup();
+    checkboxGroup2.multipleSelect = false;
+    questionWithoutCheckboxes.checkboxGroup = checkboxGroup2;
+
+    checkboxGroup.checkboxes.push(checkbox1, checkbox2, checkbox3);
+    questionCheckboxes.checkboxGroup = checkboxGroup;
+    questionGroup.questions.push(questionCheckboxes, questionWithoutCheckboxes);
+
+    this.survey.questionGroups.push(questionGroup);
   }
 
   setBasicInfoGiven(bool: boolean) {
