74c7832d9dc928e9e95c4916a9e96f100760ca09
pamei07 <p.meinhard@iks-gmbh.com>
2021-11-19 14:06:29 +0100

    Remove prefilled survey for testing

--- src/app/components/create-survey/survey-creation/survey-creation.component.ts
+++ src/app/components/create-survey/survey-creation/survey-creation.component.ts
@@ -1,9 +1,5 @@
 import {Component} from '@angular/core';
 import {Survey} from "../../../model/survey";
-import {QuestionGroup} from "../../../model/question-group";
-import {Question} from "../../../model/question";
-import {CheckboxGroup} from "../../../model/checkbox-group";
-import {Checkbox} from "../../../model/checkbox";
 
 @Component({
   selector: 'survey-creation',
@@ -17,29 +13,6 @@ export class SurveyCreationComponent {
 
   constructor() {
     this.survey = new Survey();
-    this.survey.name = "Test Umfrage";
-    this.survey.startDate = "2021-11-29T10:00:00";
-    this.survey.endDate = "2021-12-08T08:39:00";
-
-    let questionGroup = new QuestionGroup();
-    questionGroup.title = "Frageblock Beispiel";
-
-    let questionCheckboxes = new Question();
-    questionCheckboxes.text = "Beispielfrage";
-    questionCheckboxes.hasCheckbox = true;
-    let checkboxGroup = new CheckboxGroup();
-    checkboxGroup.multipleSelect = false;
-    let checkbox1 = new Checkbox();
-    checkbox1.text = "Ja";
-    let checkbox2 = new Checkbox();
-    checkbox2.text = "Nein";
-    checkboxGroup.checkboxes.push(checkbox1, checkbox2);
-    questionCheckboxes.checkboxGroup = checkboxGroup;
-
-    questionGroup.questions.push(questionCheckboxes);
-    this.survey.questionGroups.push(questionGroup);
-
-    this.basicInfoGiven = true;
   }
 
   setBasicInfoGiven(bool: boolean) {
