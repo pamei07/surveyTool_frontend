74967eaaee67b689d95d4bcc75fe699f05970c76
pamei07 <p.meinhard@iks-gmbh.com>
2021-12-06 16:11:47 +0100

    List of open access surveys sortable by start- and endDate

--- src/app/components/open-surveys/survey-list/survey-list.component.ts
+++ src/app/components/open-surveys/survey-list/survey-list.component.ts
@@ -10,6 +10,7 @@ import {Survey} from "../../../model/survey";
 export class SurveyListComponent implements OnInit {
 
   openAccessSurveys!: Survey[];
+  sortedByStartDate: boolean = true;
 
   constructor(private surveyService: SurveyService) {
   }
@@ -26,5 +27,35 @@ export class SurveyListComponent implements OnInit {
     let currentDateDF = Date.now();
 
     return currentDateDF < startDateDF;
+  }
+
+  sortByEndDate() {
+    this.openAccessSurveys.sort((survey1, survey2) => {
+      let endDate1 = Date.parse(survey1.endDate);
+      let endDate2 = Date.parse(survey2.endDate);
+      if (endDate1 < endDate2) {
+        return -1;
+      }
+      if (endDate1 > endDate2) {
+        return 1;
+      }
+      return 0;
+    })
+    this.sortedByStartDate = false;
+  }
+
+  sortByStartDate() {
+    this.openAccessSurveys.sort((survey1, survey2) => {
+      let startDate1 = Date.parse(survey1.startDate);
+      let startDate2 = Date.parse(survey2.startDate);
+      if (startDate1 < startDate2) {
+        return -1;
+      }
+      if (startDate1 > startDate2) {
+        return 1;
+      }
+      return 0;
+    })
+    this.sortedByStartDate = true;
   }
 }
