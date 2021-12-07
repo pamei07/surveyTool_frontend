e3bc562f6584df95dfba43fbc3d6df99b8637144
pamei07 <p.meinhard@iks-gmbh.com>
2021-12-07 13:01:20 +0100

    Replace question table with list and make it draggable

--- src/app/components/create-survey/question/question-list/question-list.component.ts
+++ src/app/components/create-survey/question/question-list/question-list.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {Survey} from "../../../../model/survey";
+import {CdkDragDrop, moveItemInArray} from "@angular/cdk/drag-drop";
+import {Question} from "../../../../model/question";
 
 @Component({
   selector: 'app-question-list',
@@ -10,5 +12,9 @@ export class QuestionListComponent {
 
   @Input() survey!: Survey;
   @Input() indexQuestionGroup!: number;
+
+  drop($event: CdkDragDrop<Question[]>) {
+    moveItemInArray(this.survey.questionGroups[this.indexQuestionGroup].questions, $event.previousIndex, $event.currentIndex);
+  }
 
 }
