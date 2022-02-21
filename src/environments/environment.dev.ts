8ca9db2b18701c9eb7fe7a87ee76d08905b87024
pamei07 <p.meinhard@iks-gmbh.com>
2022-02-21 14:02:58 +0100

    Add '/' to Keycloak url

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,7 +3,7 @@ export const environment = {
   baseUrl: 'http://localhost:8080/',
   frontendUrl: 'https://umfragetool-frontend-dev.herokuapp.com/',
   keycloak: {
-    issuer: 'https://umfragetool-keycloak-dev.herokuapp.com/auth',
+    issuer: 'https://umfragetool-keycloak-dev.herokuapp.com/auth/',
     realm: 'Umfragetool',
     clientId: 'surveytool-frontend'
   }
